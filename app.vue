cc586ea632a6db19c128a53a67474410a9a087e5
YuriiKyryliuk <yuriik@anybiz.io>
2023-11-27 23:17:53 +0200

    Add style to publick page

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.app
+.app(ref='content')
   NuxtLayout
     NuxtPage
 </template>
@@ -9,6 +9,35 @@ useHead({
     return titleChunk ? `${titleChunk} - Super Heroes` : 'Super Heroes'
   },
 })
+
+const content = ref()
+// const bottom = ref(false)
+ 
+function doScroll(event) {
+  console.log('Event', event)
+  // const scrollHeight = event.target.scrollHeight
+  // const scrollTop = event.target.scrollTop
+  // const clientHeight = event.target.clientHeight
+ 
+  // if (scrollTop + clientHeight >= scrollHeight) {
+  //   console.log('Yay!1')
+  //   bottom.value = true
+  // } else {
+  //   console.log('Yay!2')
+  //   bottom.value = false
+  // }
+}
+ 
+onMounted(() => {
+  // document.addEventListener('scroll', doScroll, true)
+  console.log('Content', content)
+  content.value.addEventListener("scroll", doScroll)
+})
+
+// onBeforeUnmount(() => {
+//   content.value.removeEventListener(doScroll)
+// }) 
+
 </script>
 <style lang="scss">
 *,
@@ -38,11 +67,16 @@ dl,
 dd {
   margin: 0;
 }
+
+body {
+  &::-webkit-scrollbar { 
+    display: none;
+  }
+}
 .app {
-  // background: rgb(2,6,23);
-  // background: linear-gradient(13deg, rgba(2,6,23,1) 0%, rgba(1,1,60,1) 89%, rgba(16,24,60,1) 100%);
   background: $bg_second;
   height: 100vh;
-  overflow-x: auto;
+  overflow-y: auto;
+  border: 1px solid red;
 }
 </style>
