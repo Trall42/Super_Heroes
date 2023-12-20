d667a714f96965cb19206e7fb0de87670704e0cc
YuriiKyryliuk <yuriik@anybiz.io>
2023-12-20 22:16:23 +0200

    Add styles heroes

--- pages/heroes/[id].vue
+++ pages/heroes/[id].vue
@@ -1,6 +1,17 @@
 <template lang="pug">
-.herro__container.pa-6
-  .herro__wrapper.mx-auto
+.herro__container.d-flex.justify-content-center.align-center.pa-6
+  Loader(v-if='isLoading')
+  .herro__wrapper.rounded-lg.mx-auto.pa-4
+    .herro__back
+      v-btn(
+        border
+        color='blue'
+        elevation='20'
+        variant='tonal'
+        density="compact"
+        icon="mdi-arrow-left"
+        to='/heroes'
+      )
     .d-flex
       v-img.d-flex.rounded-lg(v-if="hero?.image?.url" :src="hero?.image?.url" width="300" max-width="300" height="400" :alt="hero.name")
       .herro-parrameter.pa-6
@@ -16,13 +27,22 @@
           .d-flex.mr-4.mt-4
             v-img.mr-2(src="/images/icons/Universe.png" max-width="24" width="24" height="24" alt="icon")
             span.align-self-end {{ hero?.appearance?.race || 'N/A'}}
-    .herro__powerstats
+    .herro__powerstats.mt-14(v-if='hero?.powerstats')
       p.text-white.text-h5 Powerstats:
-        
-    .herro__biography-aliases.mt-16
+      .d-flex.justify-space-between.mx-10
+        .d-flex.flex-column.text-center(v-for='(value, name, index) in hero.powerstats' :key='index')
+          span.text-white.mb-4 {{ name }}
+          v-progress-circular(:rotate="360" :size="60" :width="4" :model-value="value" color="teal")
+            template(v-slot:default='{}') {{ value !== 'null' ? value : 0 }} %
+    .herro__biography-aliases.mt-14
       p.text-white.text-h5 Aliases:
-      .d-flex.flex-wrap.ml-10
-        span.text-white(v-for='item in hero?.biography?.aliases' :key='item') {{ item + ',&nbsp' }}
+      .d-flex.flex-wrap.mx-10(v-if='hero?.biography?.aliases.length')
+        v-chip(
+          v-for='item in hero?.biography?.aliases' :key='item'
+          color='teal'
+          class='chip ma-2'
+          variant='outlined'
+        ) {{ item }}
 </template>
 
 <script setup>
@@ -34,16 +54,20 @@ definePageMeta({
 
 const hero = ref({})
 const route = useRoute()
+const isLoading = ref(true)
 
 // ------------ function --------------
 
 async function getHero() {
   try {
+    isLoading.value = true
     const response = await nuxtApp.$api.charactersApi.getCharacters(route.params.id)
     hero.value = response.data
     console.log('Hero', response)
   } catch(error) {
     console.log('Error', error)
+  } finally {
+    isLoading.value = false
   }
 }
 
@@ -58,21 +82,33 @@ onMounted(() => {
   font-family: Inter;
   width: 100%;
   height: 100vh;
-  overflow: auto;
   background: url('/public/images/hero_background_sky.webp') no-repeat;
   background-size: 100% 100%;
   .herro__wrapper {
+    position: relative;
+    z-index: 1;
     width: 100%;
+    height: 100%;
+    max-height: 1000px;
+    overflow: auto;
     max-width: 1024px;
-    // height: 1000px;
-    border: 1px solid green;
+    height: max-content;
+    border: 2px solid $bg_first;
+    background: rgba(21, 21, 21, 0.4);
+    box-shadow: 0px 0px 20px #1877F2;
+    .herro__back {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      z-index: 10;
+    }
   }
 }
 .herro-parrameter {
   width: 100%;
 }
 .herro-parrameter__full-name,
-.herro__biography-aliases > div > span {
+.herro__biography-aliases .chip {
   font-size: $font-size-16;
   opacity: 0.75;
 }
