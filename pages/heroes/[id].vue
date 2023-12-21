e610f9bd11cecc036bfa645ac38230ea6a1f7483
YuriiKyryliuk <yuriik@anybiz.io>
2023-12-21 22:03:43 +0200

    Add style to heroes

--- pages/heroes/[id].vue
+++ pages/heroes/[id].vue
@@ -12,7 +12,7 @@
         icon="mdi-arrow-left"
         to='/heroes'
       )
-    .d-flex
+    .d-flex.flex-wrap
       v-img.d-flex.rounded-lg(v-if="hero?.image?.url" :src="hero?.image?.url" width="300" max-width="300" height="400" :alt="hero.name")
       .herro-parrameter.pa-6
         .herro-parrameter__full-name.text-white {{ hero?.biography?.[`full-name`] || 'N/A' }}
@@ -27,15 +27,24 @@
           .d-flex.mr-4.mt-4
             v-img.mr-2(src="/images/icons/Universe.png" max-width="24" width="24" height="24" alt="icon")
             span.align-self-end {{ hero?.appearance?.race || 'N/A'}}
+        .herro-publisher.text-white.d-flex.align-center.mt-4
+          span.mr-2 Publisher:
+          .herro-publisher__group.d-flex(v-if='hero?.biography?.publisher === "Binary"')
+            v-img(src="/logo/dc_logo.png" width="44" max-width="54" alt="icon")
+            v-img(src="/logo/marvel_logo.png" width="44" max-width="54" alt="icon")
+          .herro-publisher__group.d-flex(v-else-if='hero?.biography?.publisher === "DC Comics"')
+            v-img(src="/logo/dc_logo.png" width="44" max-width="54" alt="icon")
+          .herro-publisher__group.d-flex(v-else)
+            v-img(src="/logo/marvel_logo.png" width="44" max-width="54" alt="icon")
     .herro__powerstats.mt-14(v-if='hero?.powerstats')
       p.text-white.text-h5 Powerstats:
-      .d-flex.justify-space-between.mx-10
+      .d-flex.justify-space-between.flex-wrap.mx-10
         .d-flex.flex-column.text-center(v-for='(value, name, index) in hero.powerstats' :key='index')
           span.text-white.mb-4 {{ name }}
           v-progress-circular(:rotate="360" :size="60" :width="4" :model-value="value" color="teal")
             template(v-slot:default='{}') {{ value !== 'null' ? value : 0 }} %
     .herro__biography-aliases.mt-14
-      p.text-white.text-h5 Aliases:
+      p.text-white.text-h5.mt-6 Aliases:
       .d-flex.flex-wrap.mx-10(v-if='hero?.biography?.aliases.length')
         v-chip(
           v-for='item in hero?.biography?.aliases' :key='item'
@@ -43,6 +52,20 @@
           class='chip ma-2'
           variant='outlined'
         ) {{ item }}
+      p.text-white.text-h5.mt-6 Publisher:
+      .d-flex.flex-wrap.mx-10.mt-2(v-if='hero?.biography?.publisher')
+        v-chip(
+          color='teal'
+          class='chip'
+          variant='outlined'
+        ) {{ hero?.biography?.publisher }}
+      p.text-white.text-h5.mt-6 Work:
+      .d-flex.flex-wrap.mx-10.mt-2(v-if='hero?.work?.occupation')
+        v-chip(
+          color='teal'
+          class='chip multi-line'
+          variant='outlined'
+        ) {{ hero?.work?.occupation }}
 </template>
 
 <script setup>
@@ -63,39 +86,50 @@ async function getHero() {
     isLoading.value = true
     const response = await nuxtApp.$api.charactersApi.getCharacters(route.params.id)
     hero.value = response.data
-    console.log('Hero', response)
   } catch(error) {
     console.log('Error', error)
   } finally {
     isLoading.value = false
   }
 }
-
-
-onMounted(() => {
-  getHero()
-  console.log('Route', route.params.id)
-})
 </script>
 <style lang="scss">
+.v-chip {
+  padding: 4px 12px !important;
+  white-space: wrap;
+  &.v-chip--density-default {
+    min-height: max-content;
+    height: 100%;
+  }
+}
+.multi-line {
+  .v-chip__content {
+    word-break: break-all !important;
+  }
+}
 .herro__container {
   font-family: Inter;
   width: 100%;
   height: 100vh;
+  overflow: auto;
   background: url('/public/images/hero_background_sky.webp') no-repeat;
   background-size: 100% 100%;
   .herro__wrapper {
     position: relative;
     z-index: 1;
     width: 100%;
-    height: 100%;
-    max-height: 1000px;
+    height: 100% !important;
     overflow: auto;
     max-width: 1024px;
     height: max-content;
     border: 2px solid $bg_first;
     background: rgba(21, 21, 21, 0.4);
     box-shadow: 0px 0px 20px #1877F2;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    &::-webkit-scrollbar { 
+      display: none;
+    }
     .herro__back {
       position: absolute;
       top: 10px;
@@ -106,6 +140,7 @@ onMounted(() => {
 }
 .herro-parrameter {
   width: 100%;
+  max-width: max-content;
 }
 .herro-parrameter__full-name,
 .herro__biography-aliases .chip {
@@ -116,6 +151,7 @@ onMounted(() => {
   font-weight: 600;
   font-size: $font-size-40;
 }
+.herro-publisher,
 .herro-parrameter__appearance span {
   color: $white;
   font-size: $font-size-12;
